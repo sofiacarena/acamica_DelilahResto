e76944103f47587158c2a25ee5ab2d22272f905d
sofiacarena <57421202+sofiacarena@users.noreply.github.com>
2020-08-26 12:53:05 -0300

    Add files via upload
    Server.js with package.json and connection to db configuration data

---
+++ config.js
@@ -0,0 +1,16 @@
+//Connection to db data
+const NAME = 'delilahresto';
+const USER = 'root';
+const PASSWORD = '';
+const HOST = 'localhost';
+const PORT = '3306';
+const DIALECT = 'mysql';
+module.exports = {
+    NAME: NAME,
+    USER: USER,
+    PASSWORD: PASSWORD,
+    HOST: HOST,
+    PORT: PORT,
+    DIALECT: DIALECT
+}
+
